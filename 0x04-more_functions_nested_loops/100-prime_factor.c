cbd8b847271f9b24dca745134c67b068d12f99c6
emilianosavy <quenumemiline1@yahoo.com>
2022-10-21 19:57:03 -0700

    Tasks 12

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+/**
+  * main - checks prime factors of numbers
+  * prints the largest;
+  *
+  *Return: 0
+  */
+int main(void)
+{
+	long int num, a;
+
+	num = 612852475143;
+
+	for (a = 2; a <= num; a++)
+	{
+		if (num % a == 0)
+		{
+			num = num / a;
+
+			a--;
+		}
+	}
+
+	printf("%Id\n", a);
+
+	return (0);
+}
