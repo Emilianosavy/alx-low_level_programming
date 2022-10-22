346835629b0d4943763bd7188ec22d7aa1ba73f4
emilianosavy <quenumemiline1@yahoo.com>
2022-10-21 18:21:29 -0700

    fix

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,9 +14,13 @@ void print_square(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			_putchar(35);
+			for (j = 0; j < size; j++)
+			{
+				_putchar(35);
+			}
+
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 	else
 	{
