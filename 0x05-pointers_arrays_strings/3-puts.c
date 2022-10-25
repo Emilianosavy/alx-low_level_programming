b744e06a7af5207feb6b674a928c3952044ba3a4
emilianosavy <quenumemiline1@yahoo.com>
2022-10-25 02:03:38 -0700

    TASK 4

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- *_puts - prints a string to stdout
- * @str: pointer to the string to print
+ *_puts-prints a string to stdout
+ * @str:pointer to the string to print
  */
-void _puts(char *str)
+
+void _putchar(char *str)
 {
 	int i;
+	int _putchar;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
+ 		_putchar(str[i]);
 	}
-	_putchar('\n');
+		_putchar('\n');
 }
