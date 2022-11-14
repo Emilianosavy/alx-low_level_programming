7ea3f646ed3df9187f33988ecdfba567a0a62431
emilianosavy <quenumemiline1@yahoo.com>
2022-11-14 13:34:13 -0800

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -The name of the file it was compiled from will be printed
+ *
+ * Return: 0
+ */
+int main(void)
+{
+		printf("%s\n", __FILE__);
+
+			return (0);
+}
