0ea967097adb31ef7c2fde72e4298eff8ae5c19f
emilianosavy <quenumemiline1@yahoo.com>
2023-01-12 22:48:26 +0100

    mandatory/advanced tasks

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints elements of a dlistint_t list
+ * dlistint_len - returns thenumber of nodes in a dlistint_t list
  * @h: pointer to head
  *
  * Return: number of nodes
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	const dlistint_t *temp = h;
 	int count = 0;
 
 	for (count = 0; temp; temp = temp->next, count++)
-		printf("%d\n", temp->n);
+		;
 
 	return (count);
 }
